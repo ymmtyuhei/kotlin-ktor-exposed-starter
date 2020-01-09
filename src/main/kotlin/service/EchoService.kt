ebdd9d11720d8b67f451c87b0f2dd6c046b04d03
Yuhei Yamamoto <>
2020-01-09 13:18:39 +0900

    fix of `cannot be executed; I/O reactor status: STOPPED`

--- src/main/kotlin/service/EchoService.kt
+++ src/main/kotlin/service/EchoService.kt
@@ -6,40 +6,39 @@ import io.ktor.client.features.DefaultRequest
 import io.ktor.client.features.json.GsonSerializer
 import io.ktor.client.features.json.JsonFeature
 import io.ktor.client.request.get
-import model.*
-import org.jetbrains.exposed.sql.*
 
 class EchoService {
 
     private val mUrl = "https://postman-echo.com/get?foo1=bar1&foo2=bar2"
 
     suspend fun getEcho() : String{
-        return httpClient.use { client ->
+        return client().use { client ->
             client.get(mUrl)
         }
     }
 
-    private val httpClient = HttpClient(Apache) {
-        engine {
-            followRedirects = true
-            socketTimeout = 10_000
-            connectTimeout = 10_000
-            connectionRequestTimeout = 20_000
-            customizeClient {
-                setMaxConnTotal(10)  //setMaxConnTotal(1000)
-                setMaxConnPerRoute(5)  //setMaxConnPerRoute(100)
+    private fun client():HttpClient {
+        return HttpClient(Apache) {
+            engine {
+                followRedirects = true
+                socketTimeout = 10_000
+                connectTimeout = 10_000
+                connectionRequestTimeout = 20_000
+                customizeClient {
+                    setMaxConnTotal(10)  //setMaxConnTotal(1000)
+                    setMaxConnPerRoute(5)  //setMaxConnPerRoute(100)
+                }
             }
-        }
-        install(DefaultRequest) {
-            // headers.appendAuth()
-        }
-        install(JsonFeature) {
-            serializer = GsonSerializer {
-                // serializeNulls()
-                disableHtmlEscaping()
+            install(DefaultRequest) {
+                // headers.appendAuth()
+            }
+            install(JsonFeature) {
+                serializer = GsonSerializer {
+                    // serializeNulls()
+                    disableHtmlEscaping()
+                }
             }
         }
     }
-
 
 }
